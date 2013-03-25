fbc1f28083cacff47a6c47fa5e49d8f14107fb9d
Никита <@95.55.54.117>
2013-03-25 18:10:35 +0400

    Initial commit for pc version

---
+++ src/ru/androdev/countbench/MainClass.java
@@ -0,0 +1,19 @@
+package ru.androdev.countbench;
+
+import javax.swing.JOptionPane;
+ 
+public class MainClass {
+ 
+  public static void main(String[] args) {
+	  long before = System.currentTimeMillis();
+	  for (int x = 0; x<90000; x++)
+			{double	y = (float) (x * 2.54 + 1234);
+		    System.out.println(y);
+		}
+	  long after = System.currentTimeMillis();
+	  long diff = after - before;
+	  String str = Long.toString(diff);
+	  JOptionPane.showMessageDialog(null, "Время: "+str+" мс");
+  }
+ 
+}
